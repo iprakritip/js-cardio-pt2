a3bd6e7caade0a60d73985cdb3ad029aff365ee1
Prakriti Pokharel <iprakritip@gmail.com>
2023-04-26 09:34:11 +0545

    practice array.some

--- index.js
+++ index.js
@@ -5,18 +5,28 @@ const people = [
     { name: 'Kait', year: 1986 },
     { name: 'Irv', year: 1970 },
     { name: 'Lux', year: 2015 }
-  ];
+];
 
-  const comments = [
+const comments = [
     { text: 'Love this!', id: 523423 },
     { text: 'Super good', id: 823423 },
     { text: 'You are the best', id: 2039842 },
     { text: 'Ramen is my fav food ever', id: 123523 },
     { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+];
 
-  // Some and Every Checks
-  // Array.prototype.some() // is at least one person 19 or older?
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// check if one or more person is 19 or older
+
+// const adults = people.some((person) => {
+//     const now = new Date
+//     const curentYear = now.getFullYear();
+//     console.log(curentYear);
+//     return curentYear - person.year >= 19
+// })
+// console.log(adults)
+
   // Array.prototype.every() // is everyone 19 or older?
 
   // Array.prototype.find()
